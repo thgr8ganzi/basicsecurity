622bfebdff5ba52ab08f878ea8d88d8d1784d994
leejisoo <leejisoo@atnsgroup.com>
2022-06-09 09:54:28 +0900

    스프링시큐리티 연습

---
+++ src/main/java/io/security/basicsecurity/etc/SecurityController.java
@@ -0,0 +1,66 @@
+//package io.security.basicsecurity;
+//
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.context.SecurityContext;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import javax.servlet.http.HttpSession;
+//
+//@RestController
+//public class SecurityController {
+//
+//    @GetMapping("/")
+//    public String index(HttpSession session){
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        SecurityContext context = (SecurityContext) session.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
+//        Authentication authentication1 = context.getAuthentication();
+//        return "home";
+//    }
+//    @GetMapping("/thread")
+//    public String thread(){
+//        new Thread(
+//                new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//                    }
+//                }
+//        ).start();
+//        return "thread";
+//    }
+//
+//    @GetMapping("loginPage")
+//    public String loginPage(){
+//        return "loginPage";
+//    }
+//
+//    @GetMapping("/user")
+//    public String user(){
+//        return "user";
+//    }
+//
+//    @GetMapping("/admin/pay")
+//    public String adminPay(){
+//        return "adminPay";
+//    }
+//
+//    @GetMapping("/admin/**")
+//    public String admin(){
+//        return "admin";
+//    }
+//
+//    @GetMapping("/login")
+//    public String login(){
+//        return "login";
+//    }
+//
+//    @GetMapping("/denied")
+//    public String denied(){
+//        return "Access is denied";
+//    }
+//
+//
+//}
